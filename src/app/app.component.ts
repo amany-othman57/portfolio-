4795e8da75ffbda0c7b11376c3afe36a21ec076b
amany-othman <amanyothman57@gmail.com>
2026-03-05 05:39:29 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./components/home/home/home.component";
+import { AboutComponent } from "./components/about/about/about.component";
+import { ProjectsComponent } from "./components/projects/projects/projects.component";
+import { ContactComponent } from "./components/contact/contact/contact.component";
+import { FooterComponent } from "./components/footer/footer/footer.component";
+import { NavbarComponent } from "./components/navbar/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, AboutComponent, ProjectsComponent, ContactComponent, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
+import { AboutComponent } from './components/about/about/about.component';
+import { HomeComponent } from './components/home/home/home.component';
 import { Routes } from '@angular/router';
+import { ProjectsComponent } from './components/projects/projects/projects.component';
+import { ContactComponent } from './components/contact/contact/contact.component';
+import { NotfoundComponent } from './components/notfound/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, title: 'home' },
+  { path: 'about', component: AboutComponent, title: 'about' },
+  { path: 'projects', component: ProjectsComponent, title: 'projects' },
+  { path: 'contact', component: ContactComponent, title: 'contact' },
+  { path: '**', component: NotfoundComponent },
+];
